51fc25e8778dfb312bb804ae2184ed03af3b4bf6
brian d foy <brian.d.foy@gmail.com>
2021-01-10 00:01:25 -0500

    Update copyright dates

--- lib/App/Module/Lister.pm
+++ lib/App/Module/Lister.pm
@@ -279,7 +279,7 @@ Some bits stolen from C<mldistwatch> in the PAUSE code, by Andreas König.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2007-2018, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2007-2021, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic 2 license.
 
